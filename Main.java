8c1a16c56cca1bff930ee2db1eb32241550c2052
m4REK <vukadinovic.m@live.com>
2014-04-29 22:13:06 +0200

    Verweisung auf Referenten.java Konstruktor  in der Main.java Klasse geädert

--- Main.java
+++ Main.java
@@ -4,14 +4,16 @@ public class Main {
 		Teilnehmer teilnehmer = new Teilnehmer("Marko Vukadinovic",
 				"vukadinovic.m@live.com", "Student");
 		System.out.println(teilnehmer.toString());
+
 		Workshops wShops = new Workshops("Workshop New-Ideas", "22.03.2014");
 		wShops.addTeilnehmer(teilnehmer);
 		wShops.addTeilnehmer(new Teilnehmer("Giulia De Bilio",
 				"giuliett@gmx.de", "Privatperson"));
 		System.out.println(wShops.toString());
+
 		Referenten referent = new Referenten("Andreas Plass",
-				"andreas.plass@haw-hamburg.de", "HAW-HAMBURG", new Workshops(
-						"Mobile Games", "13.11.2014"));
+				"andreas.plass@haw-hamburg.de", "HAW-HAMBURG");
+		referent.addWorkshop(new Workshops("Mobile Games", "11.08.2014"));
 		System.out.println(referent.toString());
 
 	}
