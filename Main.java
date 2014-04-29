fb08847d1a75716a03c9bdd957fd16bb2b4032d1
m4REK <vukadinovic.m@live.com>
2014-04-29 22:57:22 +0200

    Weitere Tests durchgeführt

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Teilnehmer teilnehmer = new Teilnehmer("Marko Vukadinovic",
+		Teilnehmer marko = new Teilnehmer("Marko Vukadinovic",
 				"vukadinovic.m@live.com", "Student");
-		System.out.println(teilnehmer.toString());
+		System.out.println(marko.toString());
+
+		Teilnehmer massimo = new Teilnehmer("Massimo De Bilio",
+				"massi@mail.de", "Privatperson");
 
 		Workshops wShops = new Workshops("Workshop New-Ideas", "22.03.2014");
-		wShops.addTeilnehmer(teilnehmer);
+		wShops.addTeilnehmer(marko);
 		wShops.addTeilnehmer(new Teilnehmer("Giulia De Bilio",
 				"giuliett@gmx.de", "Privatperson"));
 		System.out.println(wShops.toString());
@@ -15,6 +18,12 @@ public class Main {
 				"andreas.plass@haw-hamburg.de", "HAW-HAMBURG");
 		referent.addWorkshop(new Workshops("Mobile Games", "11.08.2014"));
 		System.out.println(referent.toString());
+
+		Rahmenprogramme rpMusical = new Rahmenprogramme("Musical", "22.6.2014",
+				"11:30", "Hamburg");
+		rpMusical.addTeilnehmer(marko);
+		rpMusical.addTeilnehmer(massimo);
+		System.out.println(rpMusical.toString());
 
 	}
 }
